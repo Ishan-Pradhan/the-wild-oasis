4bd0371c02bf2cc1f0aedcd21d1a83855a422682
Ishan-Pradhan <pradhan.ishan70@gmail.com>
2024-04-18 09:46:32 +0545

    sidebar and backend

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://mkzkigvinxxwwtvmlhlr.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1remtpZ3Zpbnh4d3d0dm1saGxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM0MDc4NDIsImV4cCI6MjAyODk4Mzg0Mn0.3G6UpSJ2fd7BPZbuYVnBBxUeqkBBdmp2K8ti0PkLgzk";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
