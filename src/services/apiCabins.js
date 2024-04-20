4ef6626f03308139a99278ec73c0b187f203ae49
Ishan-Pradhan <pradhan.ishan70@gmail.com>
2024-04-20 17:07:08 +0545

    react query - form handling

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -8,3 +8,23 @@ export async function getCabins() {
   }
   return data;
 }
+
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+  if (error) {
+    console.log(error);
+    throw new Error("cabins could not be created");
+  }
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.log(error);
+    throw new Error("cabins could not be deleted");
+  }
+  return data;
+}
