272de1b1119ab12e5958e329e04e352e8ab16e6a
Ishan-Pradhan <pradhan.ishan70@gmail.com>
2024-04-28 14:26:09 +0545

    API-Side Sorting: Sorting Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -11,13 +11,18 @@ export function useBookings() {
       ? null
       : { field: "status", value: filterValue };
 
+  //Sort
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter], //for refetching
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy], //for refetching
+    queryFn: () => getBookings({ filter, sortBy }),
   });
   return { isLoading, bookings, error };
 }
