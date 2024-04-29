c2ec82c478837ff3e916c633734cd66833a5c795
Ishan-Pradhan <pradhan.ishan70@gmail.com>
2024-04-29 22:38:18 +0545

    user login

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -3,12 +3,19 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
+import SpinnerMini from "../../ui/SpinnerMini";
+import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("ishanpradhan70@gmail.com");
+  const [password, setPassword] = useState("ishan123");
+  const { login, isLoading } = useLogin();
 
-  function handleSubmit() {}
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+    login({ email, password });
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -20,6 +27,7 @@ function LoginForm() {
           autoComplete="username"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoading}
         />
       </FormRowVertical>
       <FormRowVertical label="Password">
@@ -32,7 +40,9 @@ function LoginForm() {
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large">Login</Button>
+        <Button size="large" disabled={isLoading}>
+          {!isLoading ? "Login" : <SpinnerMini />}
+        </Button>
       </FormRowVertical>
     </Form>
   );
--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -13,6 +13,11 @@ const Heading = styled.h1`
     props.as === "h3" &&
     `font-size: 2rem;
   font-weight: 500;`}
+  ${(props) =>
+    props.as === "h4" &&
+    `font-size: 3rem;
+  font-weight: 600;
+  text-align: center`}
 `;
 
 export default Heading;
